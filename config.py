7426d0d6dea6856d76e803eaf09f26a062e9b979
Alen Subasic <alen.subasic@dhigroupinc.com>
2024-10-18 16:26:34 -0500

    docker action update

--- config.py
+++ config.py
@@ -3,11 +3,18 @@
 import os
 
 RADIO_NAME = os.getenv("RADIO_NAME", "Basic Radio")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "default_api_key")
-OPENAI_CHAT_API_URL = os.getenv("OPENAI_CHAT_API_URL", "https://api.openai.com/v1/chat/completions")
-OPENAI_TTS_API_URL = os.getenv("OPENAI_TTS_API_URL", "https://api.openai.com/v1/audio/speech")
+OPENAI_API_KEY = os.getenv(
+    "OPENAI_API_KEY",
+    "",
+)
+OPENAI_CHAT_API_URL = os.getenv(
+    "OPENAI_CHAT_API_URL", "https://api.openai.com/v1/chat/completions"
+)
+OPENAI_TTS_API_URL = os.getenv(
+    "OPENAI_TTS_API_URL", "https://api.openai.com/v1/audio/speech"
+)
 OPENAI_VOICE = os.getenv("OPENAI_VOICE", "nova")
-ICECAST_HOST = os.getenv("ICECAST_HOST", "localhost")
+ICECAST_HOST = os.getenv("ICECAST_HOST", "10.10.10.5")
 ICECAST_PORT = int(os.getenv("ICECAST_PORT", 8000))
 ICECAST_USER = os.getenv("ICECAST_USER", "AI_RADIO_MASINA")
 ICECAST_PASSWORD = os.getenv("ICECAST_PASSWORD", "Fildo198&")
@@ -18,10 +25,15 @@ ICECAST_SAMPLERATE = int(os.getenv("ICECAST_SAMPLERATE", 44100))
 ICECAST_CHANNELS = int(os.getenv("ICECAST_CHANNELS", 2))
 LIQUIDSOAP_HOST = os.getenv("LIQUIDSOAP_HOST", "localhost")
 LIQUIDSOAP_PORT = int(os.getenv("LIQUIDSOAP_PORT", 7001))
-MUSIC_DIR = os.getenv("MUSIC_DIR", "")
+MUSIC_DIR = os.getenv(
+    "MUSIC_DIR",
+    "",
+)
 INTRO_SONG_PATH = os.getenv("INTRO_SONG_PATH", "./intro_song.mp3")
 DJ_NAME = os.getenv("DJ_NAME", "DJ Mašina")
 DJ_GENDER = os.getenv("DJ_GENDER", "Female")
 DJ_DEMEANOR = os.getenv("DJ_DEMEANOR", "Fun and Energetic")
 DJ_LANGUAGE = os.getenv("DJ_LANGUAGE", "Bosnian")
-AUDIO_EXTENSIONS = tuple(os.getenv("AUDIO_EXTENSIONS", ".mp3,.m4a,.wav,.flac").split(","))
+AUDIO_EXTENSIONS = tuple(
+    os.getenv("AUDIO_EXTENSIONS", ".mp3,.m4a,.wav,.flac").split(",")
+)
--- liquidsoap_client.py
+++ liquidsoap_client.py
@@ -2,6 +2,7 @@ import requests
 from config import LIQUIDSOAP_HOST, LIQUIDSOAP_PORT
 from logger import log
 
+
 class LiquidsoapClient:
     def __init__(self, host=LIQUIDSOAP_HOST, port=LIQUIDSOAP_PORT):
         self.host = host
@@ -17,7 +18,9 @@ class LiquidsoapClient:
                 log(f"Successfully queued song: {title if title else song_path}")
                 return response.text
             else:
-                log(f"Failed to queue song: {title if title else song_path}. Status code: {response.status_code}")
+                log(
+                    f"Failed to queue song: {title if title else song_path}. Status code: {response.status_code}"
+                )
                 return None
         except Exception as e:
             log(f"Error queuing song: {e}")
